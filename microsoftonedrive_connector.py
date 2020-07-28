992565db6ff0777c4c914dfe6f7dbd63f33dcb58
mjiyani <mjiyani@splunk.com>
2020-07-28 16:02:12 +0530

    MSOneDrive: Commit for removing unnecessary parentheses

--- microsoftonedrive_connector.py
+++ microsoftonedrive_connector.py
@@ -1443,7 +1443,7 @@ if __name__ == '__main__':
     with open(args.input_test_json) as f:
         in_json = f.read()
         in_json = json.loads(in_json)
-        print((json.dumps(in_json, indent=4)))
+        print(json.dumps(in_json, indent=4))
 
         connector = MicrosoftOnedriveConnector()
         connector.print_progress_message = True
@@ -1453,6 +1453,6 @@ if __name__ == '__main__':
             connector._set_csrf_info(csrftoken, headers['Referer'])
 
         ret_val = connector._handle_action(json.dumps(in_json), None)
-        print((json.dumps(json.loads(ret_val), indent=4)))
+        print(json.dumps(json.loads(ret_val), indent=4))
 
     exit(0)
